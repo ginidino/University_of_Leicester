b890a36ca11ad828d176c8d6007a2a732639e16e
Injae Lee <94044443+ginidino@users.noreply.github.com>
2023-03-09 14:26:00 +0000

    Add files via upload

---
+++ Introduction to Object Oriented Programming/Week7/Task1/Task1.java
@@ -0,0 +1,11 @@
+package Lab07_Test;
+
+public class Task1 {
+	public static void main(String[] args) {
+		Vehicle myCar = new Car();
+        Vehicle myTruck = new Truck();
+        
+        myCar.engine();
+        myTruck.engine();
+	}
+}
